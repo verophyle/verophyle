870c18070212947fb8313625f246f1d70081cd19
Gordon Tisher <gordon.tisher@verophyle.com>
2012-11-22 21:49:26 -0800

    design work

--- Verophyle/VerophyleCore/src/main/java/com/verophyle/core/client/activity/header/HeaderActivityMapper.java
+++ Verophyle/VerophyleCore/src/main/java/com/verophyle/core/client/activity/header/HeaderActivityMapper.java
@@ -3,6 +3,7 @@ package com.verophyle.core.client.activity.header;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 import com.verophyle.core.client.activity.CoreActivityMapper;
+import com.verophyle.core.client.activity.CoreActivityProxy;
 import com.verophyle.core.client.place.CorePlace;
 
 public class HeaderActivityMapper extends CoreActivityMapper<HeaderActivity, CorePlace> {
