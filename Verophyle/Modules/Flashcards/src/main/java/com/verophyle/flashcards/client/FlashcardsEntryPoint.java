13061d19705f18bdd409d3186cc1271f8af2e2b3
Gordon Tisher <gordon.tisher@verophyle.com>
2012-12-10 11:58:10 -0800

    start of flashcards module

---
+++ Verophyle/Modules/Flashcards/src/main/java/com/verophyle/flashcards/client/FlashcardsEntryPoint.java
@@ -0,0 +1,13 @@
+package com.verophyle.flashcards.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class FlashcardsEntryPoint implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
