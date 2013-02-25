8fc2357f762c2a7d086a3958d1e10e2c225dd33c
Gordon Tisher <gordon.tisher@verophyle.com>
2013-02-25 11:23:23 -0800

    some work on selenium

---
+++ Source/Testing/IntegrationTests/src/test/java/com/verophyle/integrationtests/Service.java
@@ -0,0 +1,34 @@
+package com.verophyle.integrationtests;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+
+public abstract class Service {
+
+  private static final List<Service> ALL_SERVICES = new ArrayList<Service>();
+
+  private WebDriver driver;
+
+  protected Service() {
+  }
+
+  public WebDriver getDriver() {
+    return driver;
+  }
+
+  protected void setDriver(WebDriver driver) {
+    this.driver = driver;
+  }
+
+  public static List<Service> getAllServices() {
+    return ALL_SERVICES;
+  }
+
+  protected static void addService(Service service) {
+    if (!ALL_SERVICES.contains(service))
+      ALL_SERVICES.add(service);
+  }
+
+}
