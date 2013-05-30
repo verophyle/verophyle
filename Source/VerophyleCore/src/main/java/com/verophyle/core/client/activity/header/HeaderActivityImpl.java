25f9b98fb684f9bb0179c32a8fa530cdc88ffcb1
Gordon Tisher <gordon.tisher@verophyle.com>
2013-05-30 16:46:09 -0700

    case 40: getting rid of auth popup

--- Source/VerophyleCore/src/main/java/com/verophyle/core/client/activity/header/HeaderActivityImpl.java
+++ Source/VerophyleCore/src/main/java/com/verophyle/core/client/activity/header/HeaderActivityImpl.java
@@ -10,34 +10,23 @@ import java.util.logging.Level;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.LoadEvent;
-import com.google.gwt.event.dom.client.LoadHandler;
 import com.google.gwt.http.client.UrlBuilder;
 import com.google.gwt.place.shared.PlaceController;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.AcceptsOneWidget;
-import com.google.gwt.user.client.ui.Frame;
 import com.google.inject.Inject;
 import com.google.web.bindery.requestfactory.shared.Receiver;
 import com.google.web.bindery.requestfactory.shared.Request;
 import com.google.web.bindery.requestfactory.shared.ServerFailure;
 import com.verophyle.core.client.CoreLogger;
-import com.verophyle.core.client.CoreUtil;
 import com.verophyle.core.client.activity.CoreActivityImpl;
-import com.verophyle.core.client.event.LoginEvent;
-import com.verophyle.core.client.event.LoginEvent.LoginEventHandler;
-import com.verophyle.core.client.event.LogoutEvent;
-import com.verophyle.core.client.event.LogoutEvent.LogoutEventHandler;
 import com.verophyle.core.client.place.CorePlace;
 import com.verophyle.core.client.place.Index;
 import com.verophyle.core.client.view.header.HeaderView;
-import com.verophyle.core.client.widgets.AuthenticationPopup;
 import com.verophyle.core.client.widgets.IdentityAuthentication;
-import com.verophyle.core.shared.AuthResult;
 import com.verophyle.core.shared.CoreMessages;
 import com.verophyle.core.shared.rf.CoreRequestFactory;
 import com.verophyle.core.shared.rf.CoreUserProxy;
-import com.verophyle.core.shared.rf.identity.AuthIdentityResultProxy;
 import com.verophyle.core.shared.rf.identity.IdentityProxy;
 import com.verophyle.core.shared.rf.identity.IdentityRequest;
 
@@ -46,19 +35,14 @@ import com.verophyle.core.shared.rf.identity.IdentityRequest;
  */
 public class HeaderActivityImpl extends CoreActivityImpl<CorePlace, HeaderView> implements HeaderActivity {
 
-  private final com.google.web.bindery.event.shared.EventBus eventBus;
   private final CoreMessages coreMessages;
   private final CoreRequestFactory requestFactory;
   private final HeaderView headerView;
-
-  private AuthenticationPopup popup = null;
-  private String nonce = null;
 
   private static final List<String>  WHITELIST = Arrays.asList("gordon.tisher@gmail.com", "gordon.tisher@verophyle.com");
 
   @Inject
   public HeaderActivityImpl(
-      com.google.web.bindery.event.shared.EventBus eventBus, 
       CoreLogger logger, 
       CoreMessages coreMessages, 
       CoreRequestFactory requestFactory,
@@ -66,7 +50,6 @@ public class HeaderActivityImpl extends CoreActivityImpl<CorePlace, HeaderView> 
       HeaderView headerView) {
     super(logger, placeController, headerView);
 
-    this.eventBus = eventBus;
     this.coreMessages = coreMessages;
     this.requestFactory = requestFactory;
     this.headerView = headerView;
@@ -77,7 +60,7 @@ public class HeaderActivityImpl extends CoreActivityImpl<CorePlace, HeaderView> 
     super.start(panel, eventBus);
 
     initHandlers(eventBus);
-    initAuthInfo();
+    displayAuthInfo();
   }
 
   private void initHandlers(com.google.gwt.event.shared.EventBus eventBus) {
@@ -96,21 +79,6 @@ public class HeaderActivityImpl extends CoreActivityImpl<CorePlace, HeaderView> 
         onLoginClick();
       }
     });
-
-    // login / logout events
-    eventBus.addHandler(LoginEvent.TYPE, new LoginEventHandler() {
-      @Override
-      public void onLoginEvent(LoginEvent event) {
-        onLogin();
-      }
-    });
-
-    eventBus.addHandler(LogoutEvent.TYPE, new LogoutEventHandler() {
-      @Override
-      public void onLogoutEvent(LogoutEvent event) {
-        onLogout();
-      }
-    });
   }
 
   private void onLogoClick() {
@@ -118,84 +86,11 @@ public class HeaderActivityImpl extends CoreActivityImpl<CorePlace, HeaderView> 
   }
 
   private void onLoginClick() {
-    final String url = headerView.getIdentityAuth().getUrl();
-
-    if (url != null && popup == null) {
-      this.popup = new AuthenticationPopup(url);
-      final Frame frame = popup.getFrame();
-
-      if (frame == null) {
-        popup.center();
-        return;
-      }
-
-      // add load handler to frame
-      frame.addLoadHandler(new LoadHandler() {
-
-        @Override
-        public void onLoad(LoadEvent event) {
-          final IdentityRequest identityRequest = requestFactory.identityRequest();
-          identityRequest.getLoggedInIdentity(nonce).with("identity")
-            .fire(new Receiver<AuthIdentityResultProxy>() {
-
-            @Override
-            public void onSuccess(AuthIdentityResultProxy response) {
-              if (response != null && response.getResult() == AuthResult.SUCCESS) {
-                switch (response.getAction()) {
-                case NONE:
-                  return; // leave the popup alone
-                case LOGIN:
-                  if (!response.getIdentity().isAnonymous())
-                    fireLoginEvent();
-                  break;
-                case LOGOUT:
-                  if (response.getIdentity().isAnonymous())
-                    fireLogoutEvent();
-                  break;
-                }
-                
-                hidePopup();
-              }
-            }
-
-            @Override
-            public void onFailure(ServerFailure error) {
-              super.onFailure(error);
-              log(Level.SEVERE, "failed to get logged-in identity: " + error.getMessage() + "\n" + error.getStackTraceString());
-              hidePopup();
-            }
-          });
-        }
-        
-      });
-      
-      popup.center();
-    }
+    final IdentityAuthentication auth = headerView.getIdentityAuth();
+    Window.Location.replace(auth.getUrl());
   }
 
-  private void hidePopup() {
-    nonce = null;
-    popup.hide();
-    popup = null;    
-  }
-  
-  private void fireLoginEvent() {
-    eventBus.fireEvent(new LoginEvent());
-  }
-
-  private void fireLogoutEvent() {
-    eventBus.fireEvent(new LogoutEvent());
-  }
-
-  private void onLogin() {
-    initAuthInfo();
-  }
-
-  private void onLogout() {
-    initAuthInfo();
-  }
-
-  private void initAuthInfo() {
+  private void displayAuthInfo() {
     final IdentityRequest request = requestFactory.identityRequest();
     final Request<IdentityProxy> currentIdentity = request.getCurrentIdentity().with("users");
 
@@ -276,7 +171,7 @@ public class HeaderActivityImpl extends CoreActivityImpl<CorePlace, HeaderView> 
     final IdentityAuthentication auth = headerView.getIdentityAuth();
     auth.getIdentityLogin().setText(coreMessages.login());
 
-    final String destination = getDestinationUrl("login");
+    final String destination = getDestinationUrl();
 
     requestFactory.identityRequest().getLoginUrl(destination).fire(new Receiver<String>() {
       @Override
@@ -293,7 +188,7 @@ public class HeaderActivityImpl extends CoreActivityImpl<CorePlace, HeaderView> 
     final IdentityAuthentication auth = headerView.getIdentityAuth();
     auth.getIdentityLogin().setText(coreMessages.logout());
 
-    final String destination = getDestinationUrl("logout");
+    final String destination = getDestinationUrl();
 
     requestFactory.identityRequest().getLogoutUrl(destination).fire(new Receiver<String>() {
       @Override
@@ -306,14 +201,8 @@ public class HeaderActivityImpl extends CoreActivityImpl<CorePlace, HeaderView> 
     });
   }
 
-  private String getDestinationUrl(String action) {
-    nonce = CoreUtil.genUuid();
-
+  private String getDestinationUrl() {
     final UrlBuilder builder = Window.Location.createUrlBuilder();
-    builder.setPath("Authentication");
-    builder.setParameter("action", action);
-    builder.setParameter("nonce", nonce);
-    builder.setParameter("gwt.codesvr", (String) null);
     return builder.buildString();
   }
 
